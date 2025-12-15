45086650bf00dbf408b9846dd3e6146aedeb19ef
aandersonruth2-code <aandersonruth2@gmail.com>
2025-12-15 19:10:32 +0100

    add section update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import Hero from "../components/Hero";
 import Services from "../components/Services";
 import Pricing from "../components/Pricing";
 import Aboutus from "../components/Aboutus";
+import Contact from "../components/us1J";
 import Map from "../components/Map";
 import Contact from "../components/Contact";
-import Contact from "../components/us1J";
+
 
 export default function Home() {
   return (
